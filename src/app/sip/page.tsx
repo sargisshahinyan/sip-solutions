2881972cbddc9642c431d0237d90611246ad3d97
sargisshahinyan <shahinyan.sargis@gmail.com>
2024-11-20 21:05:39 +0100

    feat: Show sip solutions splaceholder

--- src/app/sip/page.tsx
+++ src/app/sip/page.tsx
@@ -1,12 +1,5 @@
-import { Button } from "@/shared/components/Button";
-
 import styles from "./styles.module.scss";
 
-export default function Home() {
-  return (
-    <div>
-      <h1 className={styles["some-class"]}>Home page</h1>
-      <Button>Hello me</Button>
-    </div>
-  );
+export default function Placeholder() {
+  return <iframe className={styles["full-size"]} src="https://sipsolutionsip.com/" />;
 }

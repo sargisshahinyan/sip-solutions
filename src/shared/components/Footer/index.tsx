71a1fb3516a327e84650ff589bb2140ffe697f3c
sargisshahinyan <shahinyan.sargis@gmail.com>
2024-11-25 23:42:47 +0100

    remove unused variable

--- src/shared/components/Footer/index.tsx
+++ src/shared/components/Footer/index.tsx
@@ -1,9 +1,6 @@
 import { ReactNode } from "react";
-import classNames from "classnames/bind.js";
 
 import styles from "./styles.module.scss";
-
-const cx = classNames.bind(styles);
 
 export const Footer = ({}: { className?: string; children?: ReactNode }) => {
   return (

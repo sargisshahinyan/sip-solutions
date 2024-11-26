aecd85fe5c781976c63e924bc2616597cef857a6
tam-sah <tamsah18@gmail.com>
2024-11-27 00:15:14 +0100

    starting why choose us page

--- src/app/sip/why-choose-us/page.tsx
+++ src/app/sip/why-choose-us/page.tsx
@@ -1,5 +1,16 @@
 import styles from "./styles.module.scss";
+import { Navbar } from "@/shared/components/Navbar";
+import { Footer } from "@/shared/components/Footer";
+import { PageTitle } from "@/shared/components/PageTitle";
 
 export default function WhyChooseUsPage() {
-  return <h1 className={styles["why-choose-us"]}>Why choose us page</h1>;
+  return (
+    <>
+      <Navbar />
+      <main className={styles["why-choose-us"]}>
+        <PageTitle>Why choose Us?</PageTitle>
+      </main>
+      <Footer />
+    </>
+  );
 }

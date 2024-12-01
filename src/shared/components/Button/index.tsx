8d0fdaab32cfe5d4be52e4dd898a632a8503f871
sargisshahinyan <shahinyan.sargis@gmail.com>
2024-12-01 18:52:01 +0100

    Update button component to pass extra props if needed

--- src/shared/components/Button/index.tsx
+++ src/shared/components/Button/index.tsx
@@ -1,13 +1,13 @@
-import { ReactNode } from "react";
+import { ComponentProps } from "react";
 import classNames from "classnames/bind.js";
 
 import styles from "./styles.module.scss";
 
 const cx = classNames.bind(styles);
 
-export const Button = ({ className = "", children }: { className?: string; children?: ReactNode }) => {
+export const Button = ({ className = "", children, ...props }: ComponentProps<"button">) => {
   return (
-    <button type="button" className={cx("button", className)}>
+    <button className={cx("button", className)} {...props}>
       {children}
     </button>
   );

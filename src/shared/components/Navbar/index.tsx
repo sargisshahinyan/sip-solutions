6c211c994961b18d74cc9be8e824cd66cc521757
sargisshahinyan <shahinyan.sargis@gmail.com>
2024-12-08 12:55:34 +0100

    Move nav items data to separate file

--- src/shared/components/Navbar/index.tsx
+++ src/shared/components/Navbar/index.tsx
@@ -4,6 +4,8 @@ import classNames from "classnames/bind";
 
 import { Button } from "@/shared/components/Button";
 
+import { navItems } from "@/shared/components/Navbar/navItems";
+
 import logo from "../../../app/assets/images/logo.png";
 
 import styles from "./styles.module.scss";
@@ -25,51 +27,13 @@ export const Navbar = ({ className = "", isTransparent = false }: { className?: 
       <div className={cx("menu", "flex-spb-c")}>
         <nav className={cx("menu-items", "flex-spb-c")}>
           <ul className={cx("menu-items__list", "flex-spb-c")}>
-            <li className={cx("menu-items__list-item")}>
-              <Link className={cx("menu-items__list-item-link")} href="/sip/about">
-                About
-              </Link>
-            </li>
-            <li className={cx("menu-items__list-item")}>
-              <Link className={cx("menu-items__list-item-link")} href="/sip/why-choose-us">
-                Why choose Us?
-              </Link>
-            </li>
-            <li className={cx("menu-items__list-item")}>
-              <Link className={cx("menu-items__list-item-link")} href="/sip/services-and-pricing">
-                Services & Pricing
-              </Link>
-            </li>
-            <li className={cx("menu-items__list-item")}>
-              <Link className={cx("menu-items__list-item-link")} href="/sip/signature-cocktails">
-                Signature cocktails
-              </Link>
-            </li>
-            <li className={cx("menu-items__list-item")}>
-              <Link className={cx("menu-items__list-item-link")} href="/sip/sip-team">
-                Sip team
-              </Link>
-            </li>
-            <li className={cx("menu-items__list-item")}>
-              <Link className={cx("menu-items__list-item-link")} href="/sip/gallery">
-                Gallery
-              </Link>
-            </li>
-            {/*<li className={cx("menu-items__list-item")}>*/}
-            {/*  <Link className={cx("menu-items__list-item-link")} href="/">*/}
-            {/*    Press*/}
-            {/*  </Link>*/}
-            {/*</li>*/}
-            {/*<li className={cx("menu-items__list-item")}>*/}
-            {/*  <Link className={cx("menu-items__list-item-link")} href="/">*/}
-            {/*    Blog*/}
-            {/*  </Link>*/}
-            {/*</li>*/}
-            <li className={cx("menu-items__list-item")}>
-              <Link className={cx("menu-items__list-item-link")} href="/sip/contact-us">
-                Contact us
-              </Link>
-            </li>
+            {navItems.map((item) => (
+              <li className={cx("menu-items__list-item")} key={item.title + item.href}>
+                <Link className={cx("menu-items__list-item-link")} href={item.href}>
+                  {item.title}
+                </Link>
+              </li>
+            ))}
           </ul>
         </nav>
         <div className={styles["menu-btn-content"]}>
---
+++ src/shared/components/Navbar/navItems.ts
@@ -0,0 +1,39 @@
+export const navItems = [
+  {
+    title: "About",
+    href: "/sip/about",
+  },
+  {
+    title: "Why choose Us?",
+    href: "/sip/why-choose-us",
+  },
+  {
+    title: "Services & Pricing",
+    href: "/sip/services-and-pricing",
+  },
+  {
+    title: "Signature cocktails",
+    href: "/sip/signature-cocktails",
+  },
+  {
+    title: "Sip team",
+    href: "/sip/sip-team",
+  },
+  {
+    title: "Gallery",
+    href: "/sip/gallery",
+  },
+  // {
+  //   title: "Press",
+  //   href: "/sip/press",
+  // },
+  //
+  // {
+  //   title: "Blog",
+  //   href: "/sip/blog",
+  // },
+  {
+    title: "Contact us",
+    href: "/sip/contact-us",
+  },
+];

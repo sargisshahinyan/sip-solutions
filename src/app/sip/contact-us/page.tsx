e77f0b923053ea236f8e5048327c83504eb10a3f
sargisshahinyan <shahinyan.sargis@gmail.com>
2024-12-08 12:13:23 +0100

    Create contact us page and add the form

--- src/app/sip/contact-us/page.tsx
+++ src/app/sip/contact-us/page.tsx
@@ -1,5 +1,23 @@
+"use client";
+
+import { Navbar } from "@/shared/components/Navbar";
+import { PageTitle } from "@/shared/components/PageTitle";
+import { ContactUsForm } from "@/shared/components/ContactUsForm";
+
 import styles from "./styles.module.scss";
+import { Footer } from "@/shared/components/Footer";
 
 export default function ContactUsPage() {
-  return <h1 className={styles["contact-us"]}>Contact Us page</h1>;
+  return (
+    <>
+      <Navbar />
+      <main className={styles["contact-us"]}>
+        <PageTitle>Contact us</PageTitle>
+        <div className={styles.content}>
+          <ContactUsForm className={styles.form} />
+        </div>
+      </main>
+      <Footer />
+    </>
+  );
 }
